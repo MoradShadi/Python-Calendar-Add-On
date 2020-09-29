613d8a9edf49124d889e02fdc01bacb3b11d0a24
Teo Wei Sheng <wteo0011@student.monash.edu>
2020-09-29 11:03:35 +0800

    Added test_get_future_years_events + comments housekeeping OCD

--- CalendarTest.py
+++ CalendarTest.py
@@ -1,9 +1,10 @@
 import unittest
 from unittest.mock import Mock
 import Calendar
+
+# Add other imports here if needed
 import datetime
 from dateutil.relativedelta import relativedelta
-# Add other imports here if needed
 
 
 class CalendarTest(unittest.TestCase):
@@ -11,68 +12,97 @@ class CalendarTest(unittest.TestCase):
     def test_get_upcoming_events_number(self):
         num_events = 2
         time = "2020-08-03T00:00:00.000000Z"
-        
-        mock_api = Mock()
-        events = Calendar.get_upcoming_events(mock_api, time, num_events) #creates a mock of the calendar api object so it can be varried safely
-                                                                          #instead of the actual calendar.
-        
-        self.assertEqual(
-            mock_api.events.return_value.list.return_value.execute.return_value.get.call_count, 1) # asserts that the call has been made once.
-        
-        args, kwargs = mock_api.events.return_value.list.call_args_list[0] # assigns the parameter list at index 0 (only list in this case) to
-                                                                           # variables so they can be used for comparison later.
-    
-        self.assertEqual(kwargs['maxResults'], num_events) #asserts that the number of events specified is equal to the one found at the query.
 
-        #tests if an exeption is raised when the number of events entered is less than 1
+        mock_api = Mock()
+        # Creates a mock of the calendar api object so it can be varied safely instead of the actual calendar.
+        events = Calendar.get_upcoming_events(mock_api, time, num_events)
+
+        # Asserts that the call has been made once.
+        self.assertEqual(
+            mock_api.events.return_value.list.return_value.execute.return_value.get.call_count, 1)
+
+        # Assigns the parameter list at index 0 (only list in this case) to variables so they can be used for
+        # comparison later.
+        args, kwargs = mock_api.events.return_value.list.call_args_list[0]
+
+        # Asserts that the number of events specified is equal to the one found at the query.
+        self.assertEqual(kwargs['maxResults'], num_events)
+
+        # tests if an exception is raised when the number of events entered is less than 1
         mock_api = Mock()
         with self.assertRaises(ValueError):
             Calendar.get_upcoming_events(mock_api, time, 0)
-            
+
         mock_api = Mock()
         with self.assertRaises(ValueError):
             Calendar.get_upcoming_events(mock_api, time, -1)
 
-
     # Add more test cases here
     def test_get_past_years_events(self):
-        start_time = "2020-09-28T00:00:00.000000Z"
-        num_years = 2
+        start_time = datetime.datetime.utcnow().isoformat() + 'Z'
+        num_years = 5
 
         mock_api = Mock()
-        events = Calendar.get_year_past_events(mock_api, start_time, num_years)#creates a mock of the calendar api object so it can be varried safely
-                                                                              #instead of the actual calendar.
-        args, kwargs = mock_api.events.return_value.list.call_args_list[0]# assigns the parameter list at index 0 (only list in this case) to
-                                                                        # variables so they can be used for comparison later.
+        # Creates a mock of the calendar api object so it can be varied safely instead of the actual calendar.
+        events = Calendar.get_year_past_events(mock_api, start_time, num_years)
+        # Assigns the parameter list at index 0 (only list in this case) to variables so they can be used for
+        # comparison later.
+        args, kwargs = mock_api.events.return_value.list.call_args_list[0]
 
-        # Tests that we are querying for events from at least 2 years ago
+        # Tests that we are accurately querying for events from at least 5 years ago (as specified)
         self.assertEqual(int(kwargs['timeMax'][:4]) - int(kwargs['timeMin'][:4]), num_years)
 
-        #tests if an exeption is raised when the number of events entered is less than 1
+        # Tests if an exception is raised when the number of events entered is 0 or less
         mock_api = Mock()
         with self.assertRaises(ValueError):
             Calendar.get_year_past_events(mock_api, start_time, 0)
-            
+
+        mock_api = Mock()
+        with self.assertRaises(ValueError):
+            Calendar.get_year_past_events(mock_api, start_time, -1)
+
+    def test_get_future_years_events(self):
+        start_time = datetime.datetime.utcnow().isoformat() + 'Z'
+        num_years = 2
+
+        mock_api = Mock()
+        # Creates a mock of the calendar api object so it can be varied safely instead of the actual calendar.
+        events = Calendar.get_year_past_events(mock_api, start_time, num_years)
+        # Assigns the parameter list at index 0 (only list in this case) to variables so they can be used for
+        # comparison later.
+        args, kwargs = mock_api.events.return_value.list.call_args_list[0]
+
+        # Tests that we are accurately querying for events from at least next 2 years (as specified)
+        self.assertEqual(int(kwargs['timeMax'][:4]) - int(kwargs['timeMin'][:4]), num_years)
+
+        # Tests if an exception is raised when the number of events entered is 0 or less
+        mock_api = Mock()
+        with self.assertRaises(ValueError):
+            Calendar.get_year_past_events(mock_api, start_time, 0)
+
         mock_api = Mock()
         with self.assertRaises(ValueError):
             Calendar.get_year_past_events(mock_api, start_time, -1)
 
     def test_get_specific_time_events(self):
-        #tests if an exeption is raised when year entered is less than 1
+        # tests if an exception is raised when year entered is 0 or less
         mock_api = Mock()
         with self.assertRaises(ValueError):
-            Calendar.get_specific_time_events(mock_api,0)
+            Calendar.get_specific_time_events(mock_api, 0)
 
         mock_api = Mock()
         with self.assertRaises(ValueError):
-            Calendar.get_specific_time_events(mock_api,-1)
+            Calendar.get_specific_time_events(mock_api, -1)
 
     def test_search_event(self):
         api = Calendar.get_calendar_api()
-    
-        self.assertEqual(Calendar.search_event(api,'vnjfnvjfenvjefnvienbfivbefbvhejbf'),api.events().list(calendarId='primary', q='vnjfnvjfenvjefnvienbfivbefbvhejbf').execute().get('items',[]))
-        self.assertEqual(Calendar.search_event(api,'test'),api.events().list(calendarId='primary', q='test').execute().get('items',[]))
-        self.assertEqual(Calendar.search_event(api,'123456789'),api.events().list(calendarId='primary', q='123456789').execute().get('items',[]))
+
+        # Ensures that the results obtained when querying using the self written method matches the
+        # results when querying using Google Calendar API methods
+        self.assertEqual(Calendar.search_event(api, 'vnjfnvjfenvjefnvienbfivbefbvhejbf'), api.events().list(calendarId='primary', q='vnjfnvjfenvjefnvienbfivbefbvhejbf').execute().get('items', []))
+        self.assertEqual(Calendar.search_event(api, 'test'), api.events().list(calendarId='primary', q='test').execute().get('items', []))
+        self.assertEqual(Calendar.search_event(api, '123456789'), api.events().list(calendarId='primary', q='123456789').execute().get('items', []))
+
 
 def main():
     # Create the test suite from the cases above.
